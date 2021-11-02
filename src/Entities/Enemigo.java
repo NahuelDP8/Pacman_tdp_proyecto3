02531ffc1f6a903b56ab7e9992d646a16768ce66
Federico Arias <ariasfede596@gmail.com>
2021-11-02 19:08:53 -0300

    Agregamos el visitor a pared

--- src/Entities/Enemigo.java
+++ src/Entities/Enemigo.java
@@ -1,10 +1,18 @@
 package Entities;
 
+import Visitors.Visitor;
+
 public class Enemigo extends Personaje{
 
 	public Enemigo(PairTupla p, int anc, int alt) {
 		super(p, anc, alt);
 
+	}
+
+	@Override
+	public void accept(Visitor v) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/Entities/Pared.java
+++ src/Entities/Pared.java
@@ -1,8 +1,17 @@
 package Entities;
 
+import Visitors.ParedVisitor;
+import Visitors.Visitor;
+
 public class Pared extends Entidad{
 	public Pared(PairTupla p, int anc, int alt) {
 		super(p, anc, alt);
+		v = new ParedVisitor();  
+	}
+
+	@Override
+	public void accept(Visitor v) {
+		v.visitPared(this);
 	}
 
 }
